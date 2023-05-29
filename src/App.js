1590785662488ce896f214a1ea0c0fcb0496afd1
Tomasz Kula <tomaszkulewicz@gmail.com>
2023-05-29 21:11:25 +0200

    lorem ipsum generator without css.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import data from "./data";
+import {nanoid} from "nanoid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [count, setCount] = useState(1);
+    const [text, setText] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let amount = parseInt(count)
+        setText(data.slice(0,amount))
+
+    }
+
+    return (
+        <section>
+            <h4>boring lorem ipsum</h4>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='amount'>Paragraphs: </label>
+                <input type="number"
+                       name="amount"
+                       id="amount" min='1'
+                       step='1'
+                       max='9'
+                       value={count}
+                       onChange={(e) => setCount(e.target.value)}/>
+                <br/>
+                <button className='btn' type='submit'>generate</button>
+            </form>
+            <article>
+                {text.map((item)=>{
+
+                    const newId = nanoid()
+                    console.log(newId)
+                    return (
+                        <p key={newId}>
+                            {item}
+                        </p>
+                    )
+                })}
+            </article>
+        </section>
+    );
 }
 
 export default App;
